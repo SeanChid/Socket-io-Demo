8ee4b1ee4ccc732651d698f382552391104e1ca3
SeanChid <seanchidester737@gmail.com>
2024-08-16 18:17:25 -0600

    rework of index.js in progress

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import express from 'express'
 import session from 'express-session'
 import ViteExpress from 'vite-express'
-import http from 'http'
-import initializeSocketServer from './websocket.js'
 
 const app = express()
 
@@ -16,13 +14,5 @@ app.use(session({
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
-
-const server = http.createServer(app)
-
-server.on('listening', () => {
-    console.log('Server is listening on port 3000')
-})
-
-initializeSocketServer(server)
 
 ViteExpress.listen(app, 8000, () => console.log('server is running on 8000'))
